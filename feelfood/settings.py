522f3bb5f6cb5e1729b2b726f1c6dbf50e7b7696
Saidjon <fozilovsaidjon71@gmail.com>
2025-05-05 10:12:59 +0300

    l

--- feelfood/settings.py
+++ feelfood/settings.py
@@ -133,19 +133,3 @@ class Order(models.Model):
 
     def __str__(self):
         return f"Order {self.id} - {self.name}"
-
-
-# ----------------------------------------------------------------------
-# api/serializers.py
-from rest_framework import serializers
-from .models import Product, Order
-
-class ProductSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Product
-        fields = '__all__'
-
-class OrderSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Order
-        fields = ['id', 'name', 'phone', 'address', 'items', 'subtotal', 'delivery_fee', 'total', 'created_at']
