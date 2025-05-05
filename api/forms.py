3b31880235163e72bc9efa03b0e24a58c6604e89
Saidjon <fozilovsaidjon71@gmail.com>
2025-05-05 12:33:12 +0300

    migrate our migrations 2

--- api/forms.py
+++ api/forms.py
@@ -5,4 +5,4 @@ from .models import Product
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
-        fields = ['name', 'price', 'description', 'img', 'image_url']
+        fields = ['name', 'price', 'img', 'image_url']
